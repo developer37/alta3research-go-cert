f1b392a238c8f348e3afcf2982398779efc5a448
developer37 <developer37@gmail.com>
2022-06-17 17:18:26 -0400

    Update alta3research-gocert01.go

--- alta3research-gocert01.go
+++ alta3research-gocert01.go
@@ -1,6 +1,6 @@
 package main
 // This project is to demonstrate proficiency in the GO language. A REST client written in Go will make a HTTP GET call to a NASA REST API to retrieve the current NEO's 
-// (Near Earth Objects) Ids and urls which be used to retrieve more details from the NASA site. The NEO ids and urls will be collected in a map and printed. A follow up HTTP GET //  call will be made for an NEO and its details will be printed. The code can be easily modified to print more pertinent data as well as re-packaged so that func GetNeoDetails //  can be  called by other Go programs to retrieve more details for an NEO from the NASA REST API.
+// (Near Earth Objects) Ids and urls which woul be used to retrieve more details from the NASA site. The NEO ids and urls will be collected in a map and printed. A follow up HTTP GET //  call will be made for an NEO and its details will be printed. The code can be easily modified to print more pertinent data as well as re-packaged so that func GetNeoDetails //  can be  called by other Go programs to retrieve more details for an NEO from the NASA REST API.
 //  The project demonstrates how GO can simplify the processing of complex JSON in the responses of REST HTTP calls.  
 //
 import (
